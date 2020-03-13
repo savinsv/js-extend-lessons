ad17e936e0fd6bf62f1c03f3a3df8df86dbabf99
savinsv <razi@yandex.ru>
2020-03-13 13:56:32 +0300

    Корректировка css

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
+    {id: 1, img: "http://placehold.it/200x150" , title: 'Notebook', price: 20000},
+    {id: 2, img: "http://placehold.it/200x150" , title: 'Mouse', price: 1500},
+    {id: 3, img: "http://placehold.it/200x150" , title: 'Keyboard', price: 5000},
+    {id: 4, img: "http://placehold.it/200x150" , title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (id,title, price) => {
+const renderProduct = (id,img,title, price) => {
     return `<div class="product-item" id="${id}">
                 <h3>${title}</h3>
-                <p>${price}</p>
+                <a href="#"><img src=${img} alt=${title}></a>
+                <h4>${price}</h4>
                 <button class="buy-btn">Добавить в корзину</button>
             </div>`;
 };
@@ -25,8 +26,9 @@ const renderProducts = list => {
     // }); 
     //Но вот так, по моему, лучше. Нет смысла создавать промежуточный массив
     list.forEach(item => {
-        document.querySelector('.products').insertAdjacentHTML('beforeend',renderProduct(item.id,item.title, item.price));
+        document.querySelector('.products').insertAdjacentHTML('beforeend',renderProduct(item.id,item.img,item.title, item.price));
     });
 };
-const productss = [];
+
+
 renderProducts(products);
