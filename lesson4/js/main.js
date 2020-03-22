395a20e38cef7c1b7f824e0feedf8f91c8562255
savinsv <razi@yandex.ru>
2020-03-22 20:03:43 +0300

    Задание "Регулярные выражения - замена текста"

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -246,6 +246,8 @@ class ProductItem {
 }
 
 new ProductList();
+
+//Добавляем слушатель события "click" на кнопку "Корзина"
 document.querySelector('.btn-cart').addEventListener('click',(event)=>{
   let showCart = document.querySelector('.cart-body');
  // console.log(getComputedStyle(showCart).visibility);
@@ -255,3 +257,32 @@ document.querySelector('.btn-cart').addEventListener('click',(event)=>{
     showCart.style.visibility = 'hidden';
  }
 });
+
+
+//Задание урока 4
+//Регулярные выражения
+const anyText = `One: 'Hi Mary.' Two: 'Oh, hi.'
+One: 'How are you doing?'
+Two: 'I'm doing alright. How about you?'
+One: 'Not too bad. The weather is great isn't it?'
+Two: 'Yes. It's absolutely beautiful today.'
+One: 'I wish it was like this more frequently.'
+Two: 'Me too.'
+One: 'So where are you going now?'
+Two: 'I'm going to meet a friend of mine at the department store'
+One: 'Going to do a little shopping?'
+Two: 'Yeah, I have to buy some presents for my parents.'
+One: 'What's the occasion?'
+Two: 'It's their anniversary.'
+One: 'That's great. Well, you better get going. You don't want to be late.'
+Two: 'I'll see you next time.'
+One: 'Sure.' Bye.'`;
+
+//console.log(anyText);
+//Ищет "'" по всей стоке
+let regexp = /'/g;
+//"\B'" ищет в начеле и конце строки, а "\s'" ищет одинойные символы
+let regexp2 = /\B'|\s'/g;
+// console.log(anyText.replace(regexp,'"'));
+// console.log();
+// console.log(anyText.replace(regexp2,'"'));
